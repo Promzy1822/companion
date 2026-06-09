e5be9ab82e2ae15b0cec3d604f4fa53a332bd117
Promzy1822 <promiseani135@gmail.com>
2026-06-09 23:04:34 +0100

    fix: correct AppLoader import path in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import AppLoader from "./components/AppLoader";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Navbar, { NAVBAR_HEIGHT } from "./components/Navbar";
